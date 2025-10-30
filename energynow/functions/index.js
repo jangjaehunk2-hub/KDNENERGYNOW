2027a38357ce0a14b97f507f1f924fb729a321e3
SenugPyo Choi <winner101910@gmail.com>
2025-10-30 20:55:03 +0900

    Display KEPCO power data in table on homepage
    Updated the Cloud Function to fetch a wider date range and more rows. Modified the homepage to show KEPCO power data in a table instead of only logging to the console, improving data visibility for users.

--- energynow/functions/index.js
+++ energynow/functions/index.js
@@ -8,11 +8,11 @@ const API_KEY = "2ea671893271f4e1752c6a258014c54339c040da9783555cff1014fdf0cc171
 // HTTPS 요청 처리 함수
 exports.getPowerData = functions.https.onRequest(async (req, res) => {
   // 요청 파라미터 (기간)
-  const start = req.query.stDt || "2025-01";
-  const end = req.query.edDt || "2025-02";
+  const start = req.query.stDt || "2024-01";
+  const end = req.query.edDt || "2025-01";
 
   // KEPCO API URL
-  const url = `http://apis.data.go.kr/B500001/electric/elcpPerformance/elcpPerformancelist?serviceKey=${API_KEY}&pageNo=1&numOfRows=10&stDt=${start}&edDt=${end}&_type=json`;
+  const url = `http://apis.data.go.kr/B500001/electric/elcpPerformance/elcpPerformancelist?serviceKey=${API_KEY}&pageNo=1&numOfRows=100&stDt=${start}&edDt=${end}&_type=json`;
 
   try {
     const response = await fetch(url);
@@ -28,3 +28,4 @@ exports.getPowerData = functions.https.onRequest(async (req, res) => {
     res.status(500).json({ error: "API 호출 실패" });
   }
 });
+
